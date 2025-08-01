6ae07bfc5b3d77a19a384fd13ebfdd242504c0bf
pandaomprasad <omprasadpanda1@gmail.com>
2025-08-01 15:45:05 +0530

    fix: Testing

--- app/Blog/page.jsx
+++ app/Blog/page.jsx
@@ -8,28 +8,29 @@ import {MinimalCardDemo} from "./components/cardContainer";
 
 export default function BlogPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
-      <AuroraBackground className="h-[50vh] w-full border rounded-lg shadow-lg">
-        <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="relative flex flex-col gap-4 items-center justify-center px-4 w-full h-full"
-        >
-          <div className="w-full">
-            <h2 className="text-4xl md:text-5xl font-bold text-black dark:text-white leading-snug">
-              Thoughts worth <UnderlineHighlight>sharing</UnderlineHighlight>,
-              <br />
-              strategies worth <UnderlineHighlight>trying</UnderlineHighlight>.
-            </h2>
-          </div>
-        </motion.div>
-      </AuroraBackground>
-      <MinimalCardDemo />
-    </div>
+    // <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+    //   <AuroraBackground className="h-[50vh] w-full border rounded-lg shadow-lg">
+    //     <motion.div
+    //       initial={{ opacity: 0.0, y: 40 }}
+    //       whileInView={{ opacity: 1, y: 0 }}
+    //       transition={{
+    //         delay: 0.3,
+    //         duration: 0.8,
+    //         ease: "easeInOut",
+    //       }}
+    //       className="relative flex flex-col gap-4 items-center justify-center px-4 w-full h-full"
+    //     >
+    //       <div className="w-full">
+    //         <h2 className="text-4xl md:text-5xl font-bold text-black dark:text-white leading-snug">
+    //           Thoughts worth <UnderlineHighlight>sharing</UnderlineHighlight>,
+    //           <br />
+    //           strategies worth <UnderlineHighlight>trying</UnderlineHighlight>.
+    //         </h2>
+    //       </div>
+    //     </motion.div>
+    //   </AuroraBackground>
+    //   <MinimalCardDemo />
+    // </div>
+    <div style={{ padding: "100px", fontSize: "24px" }}>Test Blog Page</div>
   );
 }
