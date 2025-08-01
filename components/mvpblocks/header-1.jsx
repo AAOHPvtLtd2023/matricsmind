f60e18a3b470ade085096afc2fac82b54807264a
pandaomprasad <omprasadpanda1@gmail.com>
2025-08-01 15:53:47 +0530

    Refactor blog structure: move blog components to new directory, update routing, and implement blog post page with dynamic content

--- components/mvpblocks/header-1.jsx
+++ components/mvpblocks/header-1.jsx
@@ -30,7 +30,7 @@ const navItems = [
     ],
   },
   { name: "About", href: "/about" },
-  { name: "Blog", href: "/tempBlog" },
+  { name: "Blog", href: "/blog" },
   { name: "Course", href: "/course" },
 ];
 
