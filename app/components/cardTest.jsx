6d58d8b8cc9e9230b9747f9abb6dcd8fb3d4fccf
pandaomprasad <omprasadpanda1@gmail.com>
2025-08-06 21:58:11 +0530

    NAme changed

--- app/components/cardTest.jsx
+++ app/components/cardTest.jsx
@@ -4,14 +4,14 @@ import CardCarousel from "../../components/ui/card-carousel";
 
 export default function CardTest() {
   const images = [
-    { src: "/images/projects/Automobile.jpg", alt: "Automobile", slug: "automobile", title: "Automobile" },
-    { src: "/images/projects/Education.jpg", alt: "Education", slug: "education", title: "Education" },
-    { src: "/images/projects/Goverment_Projects.jpg", alt: "Government Projects", slug: "government-projects", title: "Government Projects" },
-    { src: "/images/projects/Healthy_Care.jpg", alt: "Healthcare", slug: "healthcare", title: "Healthcare" },
-    { src: "/images/projects/Kids.jpg", alt: "Kids", slug: "kids", title: "Kids" },
-    { src: "/images/projects/Packers_Movers.jpg", alt: "Packers and Movers", slug: "packers-movers", title: "Packers & Movers" },
-    { src: "/images/projects/Real_Estate.jpg", alt: "Real Estate", slug: "real-estate", title: "Real Estate" },
-    { src: "/images/projects/Resturant.jpg", alt: "Restaurant", slug: "restaurant", title: "Restaurant" },
+    { src: "/images/projects/automobile.jpg", alt: "Automobile", slug: "automobile", title: "Automobile" },
+    { src: "/images/projects/education.jpg", alt: "Education", slug: "education", title: "Education" },
+    { src: "/images/projects/govermentprojects.jpg", alt: "Government Projects", slug: "government-projects", title: "Government Projects" },
+    { src: "/images/projects/healthcare.jpg", alt: "Healthcare", slug: "healthcare", title: "Healthcare" },
+    { src: "/images/projects/kids.jpg", alt: "Kids", slug: "kids", title: "Kids" },
+    { src: "/images/projects/packersmovers.jpg", alt: "Packers and Movers", slug: "packers-movers", title: "Packers & Movers" },
+    { src: "/images/projects/realestate.jpg", alt: "Real Estate", slug: "real-estate", title: "Real Estate" },
+    { src: "/images/projects/resturant.jpg", alt: "Restaurant", slug: "restaurant", title: "Restaurant" },
   ];
 
   return (
