e1e9e8ed723bfe52b107243a67a5da5b4586ac95
pandaomprasad <omprasadpanda1@gmail.com>
2025-08-26 15:51:51 +0530

    Admin Panel hide

--- WORD_LINKS.js
+++ WORD_LINKS.js
@@ -98,6 +98,76 @@ const WORD_LINKS = {
     url: "https://en.wikipedia.org/wiki/United_States",
     tooltip: "United States",
     // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  Pharmaceutical:{
+    url: "https://en.wikipedia.org/wiki/Medicine",
+    tooltip: "Pharmaceutical Industry Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400", 
+  },
+  healthcare:{
+    url: "https://en.wikipedia.org/wiki/Health",
+    tooltip: "Healthcare Industry Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  physicians:{
+    url: "https://en.wikipedia.org/wiki/Physician",
+    tooltip: "Physician Services",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  clinics:{
+    url: "https://en.wikipedia.org/wiki/Clinic",
+    tooltip: "Clinic Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  hospitals:{
+    url: "https://en.wikipedia.org/wiki/Hospital",
+    tooltip: "Hospital Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  medicalcenters:{
+    url: "https://en.wikipedia.org/wiki/Medical_center",
+    tooltip: "Medical Center Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  doctors:{
+    url: "https://simple.wikipedia.org/wiki/Doctor_(title)",
+    tooltip: "Doctor Services",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  B2B:{
+    url: "https://en.wikipedia.org/wiki/Business-to-business",
+    tooltip: "B2B Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  B2C:{
+    url: "https://en.wikipedia.org/wiki/Business-to-consumer",
+    tooltip: "B2C Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  ecommerce:{
+    url: "https://en.wikipedia.org/wiki/E-commerce",
+    tooltip: "E-commerce Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  health:{
+    url: "https://www.who.int/",
+    tooltip: "Health Industry Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  wellness:{
+    url: "https://en.wikipedia.org/wiki/Wellness",
+    tooltip: "Wellness Industry Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  AI:{
+    url: "https://en.wikipedia.org/wiki/Artificial_intelligence",
+    tooltip: "AI Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
+  },
+  AR:{
+    url: "https://en.wikipedia.org/wiki/Augmented_reality",
+    tooltip: "AR Solutions",
+    // className: "text-[#ff9100] font-semibold hover:border-yellow-400",
   }
 };
 
